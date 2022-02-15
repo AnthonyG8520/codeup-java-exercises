aeaf1ace87fe4018e57536bc1263bb641bc698ae
anthonygonzales <anthony.j.gonzales22@gmail.com>
2022-02-15 15:37:50 -0600

    syntax notes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,37 @@
+import java.sql.SQLOutput;
+
+// class - defining the class
+// HelloWorld is the name of the class and it matches the name of the .java file
 public class HelloWorld {
+
+    //method - similar toa function in jaavscript
+    //main method -> specific method in java that acts as our entry point when running java
+    public static void main (String[] args){
+        //void - method doesn't expect anything to be returned
+        // main - name of our method, main is a keyword that is going ot be searched as the entry point
+        // String[] args - passing in a string array named args
+        //print out "hello world" to the console
+        System.out.print("Hello");
+        System.out.println(" World");
+
+        int myFavoritenumber = 99;
+        System.out.println(myFavoritenumber);
+
+        String myString = "Anthony";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        boolean class = false;
+    }
+
 }
---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,73 @@
+public class SyntaxTypesVariables {
+    public static void main (String[] args){
+        //primitive data types
+        //not objects
+        //store values of a specific type
+        // byte, short, int, long, float, double, char, boolean (see chart in curriculum)
+
+        //declaring variables
+        //MUST declare the data type before assigning it
+        int myFavoritenumber = 18;
+        System.out.println(myFavoritenumber);
+
+        // let's store my favorite letter
+        char myFavoriteLetter = 'L';
+        System.out.println(myFavoriteLetter);
+
+        // boolean
+        boolean isSunny = true;
+        System.out.println(isSunny);
+
+        //good to know
+        //int -> primitive type / not an object
+        //Integer -> object / give us access to manipulate the Integer as an object
+
+        //double -> primitive
+        //Double -> object
+
+        //char -> primitve
+        //Character -> object
+
+        //why not just use double for everything
+        //it will affect hoq quickly the progrsm runs
+        //use the correct data type to be more efficient
+
+        // Strings
+        // strings are not primitive data type
+        //objects
+        // can be initialized at the same time as declaring
+        //declaring a variable of the String data type
+        String school;
+        //assing a value (initializing) the variable
+        school = "Codeup";
+        System.out.println(school);
+        //can declare multiple strings at once
+
+        String student1, student2, student3;
+        student1 = "Anthony";
+        student2 = "Anthony";
+        student3 = "Anthony";
+
+        //arithmetic expressions
+        // 3, 9
+        byte firstNum = 120;
+        byte secondNum = 120;
+        int result = firstNum + secondNum;
+        System.out.println(result);
+
+        // java 'final' keyword can't reassign the variable (similar to const in JS)
+        final String myName = "Ant";
+//        myName = "name"; --> can't change value of variable
+
+        int myInteger = 900;
+        long morePrecise = myInteger;
+        System.out.println(myInteger);
+        System.out.println(morePrecise);
+
+        double pi = 3.14159;
+        int almostPi = (int) pi;
+        System.out.println(pi);
+        System.out.println(almostPi);
+
+    }
+}
