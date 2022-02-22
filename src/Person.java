c12459e3e176a0fdb759c6e557fdea5068210f97
anthonygonzales <anthony.j.gonzales22@gmail.com>
2022-02-22 16:02:50 -0600

    EOD update

--- src/Person.java
+++ src/Person.java
@@ -15,13 +15,34 @@ public class Person {
         System.out.println(String.format("Hello from %s!", name));
     }
 
+    public Person(String inputName){
+        this.name = inputName;
+    }
+
     public static void main(String[] args) {
-        Person user = new Person();
-        user.setName("Ant");
+        Person user = new Person("Ant");
+        user.setName("Anthony");
         System.out.println(user.name);
 
         System.out.println(user.getName());
 
         user.sayHello();
-    }
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//    }
 }
--- src/util/Input.java
+++ src/util/Input.java
@@ -5,4 +5,10 @@ import java.util.Scanner;
 public class Input {
     private Scanner scanner = new Scanner(System.in);
 
+
+
+
+    public static void main(String[] args) {
+
+    }
 }
