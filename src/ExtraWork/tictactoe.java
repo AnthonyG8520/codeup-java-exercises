51e915603518525292737eff0814bbd7bd08bad8
anthonygonzales <anthony.j.gonzales22@gmail.com>
2022-03-29 15:28:28 -0500

    calculator

--- src/ExtraWork/tictactoe.java
+++ src/ExtraWork/tictactoe.java
@@ -1,5 +1,7 @@
 package ExtraWork;
 
+import java.util.Scanner;
+
 public class tictactoe {
     public static String [][] tictactoe = {
             {" ", " 1 ", " 2 ", " 3 "},
@@ -28,11 +30,86 @@ public class tictactoe {
         printGame();
 
 
-
-
-
     }
+//--------------------------------------------------------------------------------------
+    public static Scanner scan = new Scanner(System.in);
+
+    public static void add(){
+        System.out.println("Enter a number");
+        int num1 = scan.nextInt();
+        System.out.println("Enter another number");
+        int num2 = scan.nextInt();
+        System.out.println("Result: " + (num1 + num2));
+    }
+
+    public static void subtract(){
+        System.out.println("Enter a number");
+        int num1 = scan.nextInt();
+        System.out.println("Enter another number");
+        int num2 = scan.nextInt();
+        System.out.println("Result: " + (num1 - num2));
+    }
+
+    public static void divide(){
+        System.out.println("Enter a number");
+        float num1 = scan.nextFloat();
+        System.out.println("Enter another number");
+        float num2 = scan.nextFloat();
+        System.out.println("Result: " + (num1 / num2));
+    }
+
+    public static void multiply(){
+        System.out.println("Enter a number");
+        int num1 = scan.nextInt();
+        System.out.println("Enter another number");
+        int num2 = scan.nextInt();
+        System.out.println("Result: " + (num1 * num2));
+    }
+
+
+    public static void calc(){
+        System.out.println("1: Addition\n2: Subtraction\n3: Division\n4: Multiplication\n5: Quit");
+        System.out.print("Choose an operation. [By list no.]");
+        String opAns = scan.next();
+        switch(opAns){
+            case "1":
+                add();
+                break;
+            case "2":
+                subtract();
+                break;
+            case "3":
+                divide();
+                break;
+            case "4":
+                multiply();
+                break;
+            case "5":
+                return;
+            default:
+                System.out.println("That entry doesn't match anything. Try again.");
+                calc();
+        }
+        System.out.println("Another?[Y/N]");
+        String cont = scan.next();
+        if(cont.equalsIgnoreCase("y")) {
+            calc();
+        }
+        System.out.println("Goodbye");
+    }
+
+
+
+
+
+
+
     public static void main(String[] args) {
-        game();
+//        game();
+        calc();
+
+
+
+
     }
 }
