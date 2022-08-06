6c39c3e2234f676d05ddfb336c466dccc7368312
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-08-05 23:10:51 -0500

    added problem that method was for

--- src/ExtraWork/ScratchWork.java
+++ src/ExtraWork/ScratchWork.java
@@ -126,6 +126,43 @@ public class ScratchWork {
     }
 
 
+//    Given a string s consisting of lowercase English letters, find the number of consecutive triplets within s formed by unique characters.
+//    In other words, count the number of indices i, such that s[i], s[i + 1], and s[i + 2] are all pairwise distinct.
+//
+//    Example
+//
+//    For s = "abcdaaae", the output should be solution(s) = 3.
+//
+//    For i = 0, s[0] = 'a', s[1] = 'b', and s[2] = 'c', which are pairwise distinct.
+//    For i = 1, s[1] = 'b', s[2] = 'c', and s[3] = 'd', which are pairwise distinct.
+//    For i = 2, s[2] = 'c', s[3] = 'd', and s[4] = 'a', which are pairwise distinct.
+//    For i = 3, s[3] = 'd', s[4] = 'a', and s[5] = 'a', which are not pairwise distinct because s[4] and s[5] are equal.
+//    For i = 4, s[4] = 'a', s[5] = 'a', and s[6] = 'a', which are not pairwise distinct because any two of these characters are equal.
+//    For i = 5, s[5] = 'a', s[6] = 'a', and s[7] = 'e', which are not pairwise distinct because s[5] and s[6] are equal.
+//    For i = 6 or i = 7, at least one of characters s[i + 1] or s[i + 2] doesn't exist.
+//    For s = "abacaba", the output should be solution(s) = 2.
+//
+//    There are only 2 indices meeting the criteria:
+//
+//    i = 1: s[1] = 'b', s[2] = 'a', and s[3] = 'c'.
+//    i = 3: s[3] = 'c', s[4] = 'a', and s[5] = 'b'.
+//    All other triplets will contain more than one a character.
+//
+//    Input/Output
+//
+//[execution time limit] 3 seconds (java)
+//
+//[input] string s
+//
+//    A string consisting of lowercase English letters.
+//
+//    Guaranteed constraints:
+//            1 ≤ s.length ≤ 1000.
+//
+//            [output] integer
+//
+//    The number of indices i in s, such that characters s[i], s[i + 1], and s[i + 2] all exist and are pairwise distinct.
+
     public static int distinct(String s){
         int count = 0;
         Character[] pairs = new Character[3];
