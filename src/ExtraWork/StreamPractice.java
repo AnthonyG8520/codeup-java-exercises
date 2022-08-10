a45ed00769f6c541a16369b07035454b88a45b24
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-08-09 21:48:35 -0500

    looking into java streams

---
+++ src/ExtraWork/StreamPractice.java
@@ -0,0 +1,16 @@
+package ExtraWork;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamPractice {
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>(Arrays.asList("ant", "anthony", "tony", "anton"));
+
+        names.stream().forEach(n -> System.out.println(n));
+
+    }
+}
