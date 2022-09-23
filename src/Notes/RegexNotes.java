43f8c59a19611551d63b34bdc4d4b8e11cf6c2df
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-09-23 13:24:33 -0500

    regex notes

---
+++ src/Notes/RegexNotes.java
@@ -0,0 +1,31 @@
+package Notes;
+
+import java.util.regex.Pattern;
+
+public class RegexNotes {
+    public static void main(String[] args) {
+    // Regex Notes
+    // Brackets are used to find a range of characters -> [abc] same as [a-c] will find any of those three characters
+
+    // \\d will find any digit between 0 and 9 -> [\\d]
+    // \\w will find a word character -> [\\w] shorthand for [a-zA-Z_0-9]
+    // \\s will find any whitespace -> useful for removing white space using .replaceAll -> \\s
+    //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
