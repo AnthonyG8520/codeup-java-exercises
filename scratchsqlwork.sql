d0bfeffa3667de487c11ad5baa91af2ec35f3e91
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-11-11 12:38:52 -0600

    leetcode sql

--- scratchsqlwork.sql
+++ scratchsqlwork.sql
@@ -98,3 +98,34 @@ using the group by clause in a subquery would not work unless
 i put it inside the FROM section of the first subquery and
 used the FROM section to make another query and give it a table alias(the query for the list of ids with distinct start_times)
 ------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+Leetcode sql--------------------------------------------------------------------------------
+Write an SQL query to report the first login date for each player.
+
+Input:
+Activity table:
++-----------+-----------+------------+--------------+
+| player_id | device_id | event_date | games_played |
++-----------+-----------+------------+--------------+
+| 1         | 2         | 2016-03-01 | 5            |
+| 1         | 2         | 2016-05-02 | 6            |
+| 2         | 3         | 2017-06-25 | 1            |
+| 3         | 1         | 2016-03-02 | 0            |
+| 3         | 4         | 2018-07-03 | 5            |
++-----------+-----------+------------+--------------+
+Output:
++-----------+-------------+
+| player_id | first_login |
++-----------+-------------+
+| 1         | 2016-03-01  |
+| 2         | 2017-06-25  |
+| 3         | 2016-03-02  |
++-----------+-------------+
+
+SELECT player_id, min(event_date) AS first_login FROM Activity GROUP BY player_id;
+
+--------------------------------------------------------------------------------------------
