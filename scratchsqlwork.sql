3b6f21a1f61e7d554b287c0bcb83000db17a3dd3
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-11-23 14:27:12 -0600

    leetcode sql

--- scratchsqlwork.sql
+++ scratchsqlwork.sql
@@ -128,4 +128,36 @@ Output:
 
 SELECT player_id, min(event_date) AS first_login FROM Activity GROUP BY player_id;
 
+
+
+Write an SQL query to report all customers who never order anything.
+
+Input:
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output:
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+SELECT name AS Customers FROM Customers WHERE id NOT IN (SELECT customerId FROM Orders);
+
 --------------------------------------------------------------------------------------------
+
