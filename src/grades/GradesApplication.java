1bfaff97b25603c83a382c7a6c8bd02d979122da
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-11-28 15:42:23 -0600

    looking over old code and refactoring to work better

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -24,22 +24,32 @@ public class GradesApplication {
         System.out.println(students.keySet());
         System.out.print("Choose a student to see more info.");
         userInput = scan.next();
-            switch(userInput){
-                case "mushy-bee":
-                    getInfo(students.get(userInput));
-                    break;
-                case "wicked-jar":
-                    getInfo(students.get(userInput));
-                    break;
-                case "groovy-bear":
-                    getInfo(students.get(userInput));
-                    break;
-                case "mushy-sponge":
-                    getInfo(students.get(userInput));
-                    break;
-                default:
-                    System.out.println("That entry doesn't match anyhing.");
+
+//        this code block hardcodes the students that can be searched
+//            switch(userInput){
+//                case "mushy-bee":
+//                    getInfo(students.get(userInput));
+//                    break;
+//                case "wicked-jar":
+//                    getInfo(students.get(userInput));
+//                    break;
+//                case "groovy-bear":
+//                    getInfo(students.get(userInput));
+//                    break;
+//                case "mushy-sponge":
+//                    getInfo(students.get(userInput));
+//                    break;
+//                default:
+//                    System.out.println("That entry doesn't match anyhing.");
+//            }
+
+//        This code block does not restrict to students only contained in the switch case
+            if(students.containsKey(userInput)){
+                getInfo(students.get(userInput));
+            } else{
+                System.out.println("That entry doesn't match anything");
             }
+
             System.out.print("Would you like to search another student? [Yes/No]");
             cont = scan.next();
             if(cont.equalsIgnoreCase("yes")){
@@ -57,16 +67,19 @@ public class GradesApplication {
         Student stu2 = new Student("Ant");
         Student stu3 = new Student("tony");
         Student stu4 = new Student("Anty");
+        Student stu5 = new Student("antony");
 
         stu1.grades.addAll(Arrays.asList(30, 90, 70));
         stu2.grades.addAll(Arrays.asList(25, 68, 94));
         stu3.grades.addAll(Arrays.asList(93, 58, 80));
         stu4.grades.addAll(Arrays.asList(79, 53, 100));
+        stu5.grades.addAll(Arrays.asList(80, 23, 89));
 
         students.put("mushy-bee", stu1);
         students.put("wicked-jar", stu2);
         students.put("groovy-bear", stu3);
         students.put("mushy-sponge", stu4);
+        students.put("big-cheese", stu5);
 
         stu1.recordAttendance("12/20", "A");
         stu1.recordAttendance("12/21", "P");
